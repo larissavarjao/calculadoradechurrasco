fd51602e536fa9ec925dc87405ed7cdf40c4b784
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-10-06 14:59:23 -0300

    Transformação para calculadora acontecendo :D

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,43 @@
 "use strict";
-'use strict';
-
-function aparecendoChurrasco() {
-    bebidas.style.display = 'none';
-    pizza.style.display = 'none';
-}
+"use strict";
 'use strict';
 
 var capa = document.querySelectorAll('.cover-item-calc');
 var churrasco = document.querySelector('.churrasco');
 var bebidas = document.querySelector('.bebidas');
-var pizza = document.querySelector('#pizza');
+var pizza = document.querySelector('.pizza');
+var subtitle = document.querySelector('.cover-subtitle');
+var optionAll = document.querySelectorAll('.options-all');
+var optionTitle = document.querySelector('.options-title');
 
-console.log(capa);
 Array.prototype.forEach.call(capa, function (item, index, array) {
     item.addEventListener('click', function () {
-        console.log(index);
+        hiddenCoverItemCalc();
+        showOptionAll();
         if (index == 0) {
-            aparecendoChurrasco();
-            console.log('Cliquei churras!');
+            showMeat();
         } else if (index === 1) {
             aparecendoBebida();
-            console.log('Cliquei bebidas!');
         } else if (index === 2) {
             aparecendoPizza();
-            console.log('Cliquei pizza!');
         }
     });
 });
 
-function aparecendoChurrasco() {
+function hiddenCoverItemCalc() {
+    subtitle.style.display = 'none';
+    churrasco.style.display = 'none';
     bebidas.style.display = 'none';
     pizza.style.display = 'none';
+}
+
+function showOptionAll() {
+    Array.prototype.forEach.call(optionAll, function (item, index, array) {
+        item.style.opacity = 1;
+    });
+}
+
+function showMeat() {
+    optionTitle = 'Churrasco';
 }
 "use strict";
--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -1,28 +1,38 @@
 var capa = document.querySelectorAll('.cover-item-calc');
 var churrasco = document.querySelector('.churrasco');
 var bebidas = document.querySelector('.bebidas');
-var pizza = document.querySelector('#pizza');
+var pizza = document.querySelector('.pizza');
+var subtitle = document.querySelector('.cover-subtitle');
+var optionAll = document.querySelectorAll('.options-all');
+var optionTitle = document.querySelector('.options-title');
 
-console.log(capa);
 Array.prototype.forEach.call(capa, function(item, index, array) {
     item.addEventListener('click', function(){
-        console.log(index);
+        hiddenCoverItemCalc();
+        showOptionAll();
         if(index == 0 ){
-            aparecendoChurrasco();
-            console.log('Cliquei churras!');
+            showMeat();
         } else if(index === 1){
             aparecendoBebida();
-            console.log('Cliquei bebidas!');
         } else if (index === 2){
             aparecendoPizza();
-            console.log('Cliquei pizza!');
         }
     })
 })
 
-
-
-function aparecendoChurrasco(){
+function hiddenCoverItemCalc(){
+    subtitle.style.display = 'none';
+    churrasco.style.display = 'none';
     bebidas.style.display = 'none';
     pizza.style.display = 'none';
+}
+
+function showOptionAll(){
+    Array.prototype.forEach.call(optionAll, function(item, index, array){
+        item.style.opacity = 1;
+    });
+}
+
+function showMeat(){
+    optionTitle = 'Churrasco';
 }
