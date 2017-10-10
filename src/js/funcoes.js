7c67f4c7d09d579010e2c31e52195658716609b5
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-10-10 11:14:24 -0300

    site funcionando, pendente calcular e responsividade

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -5,17 +5,39 @@ var pizza = document.querySelector('.pizza');
 var subtitle = document.querySelector('.cover-subtitle');
 var optionAll = document.querySelectorAll('.options-all');
 var optionTitle = document.querySelector('.options-title');
+var optionsBottons = document.querySelectorAll('.options-bottons');
+var meatTitle = document.querySelector('.meat-title');
+var meatNumber = document.querySelector('.meat-number');
+var chickenTitle = document.querySelector('.chicken-title');
+var chickenNumber = document.querySelector('.chicken-number');
+var otherTitle = document.querySelector('.other-title');
+var otherNumber = document.querySelector('.other-number');
 
 Array.prototype.forEach.call(capa, function(item, index, array) {
     item.addEventListener('click', function(){
         hiddenCoverItemCalc();
         showOptionAll();
-        if(index == 0 ){
+        if(index === 0 ){
             showMeat();
         } else if(index === 1){
-            aparecendoBebida();
+            showBeer();
         } else if (index === 2){
-            aparecendoPizza();
+            showPizza();
+        }
+    })
+})
+
+Array.prototype.forEach.call(optionsBottons, function(item, index, array){
+    item.addEventListener('click', function(){
+        if(index === 0){
+            showCoverItemCalc();
+            hiddenOptionAll();
+        } else if(index === 1){
+            showMeat();
+        } else if (index === 2){
+            showBeer();
+        } else if(index === 3){
+            showPizza();
         }
     })
 })
@@ -30,9 +52,53 @@ function hiddenCoverItemCalc(){
 function showOptionAll(){
     Array.prototype.forEach.call(optionAll, function(item, index, array){
         item.style.opacity = 1;
+        item.style.zIndex = 9;
+    });
+}
+function showCoverItemCalc(){
+    subtitle.style.display = 'flex';
+    churrasco.style.display = 'flex';
+    bebidas.style.display = 'flex';
+    pizza.style.display = 'flex';
+}
+
+function hiddenOptionAll(){
+    Array.prototype.forEach.call(optionAll, function(item, index, array){
+        item.style.opacity = 0;
+        item.style.zIndex = 3;
     });
 }
 
 function showMeat(){
-    optionTitle = 'Churrasco';
+    optionTitle.innerHTML = 'Churrasco';
+    meatTitle.innerHTML = 'Quantidade de carne:';
+    meatNumber.innerHTML = 'XXX g';
+    chickenTitle.style.display = 'flex';
+    chickenNumber.style.display = 'flex';
+    otherTitle.style.display = 'flex';
+    otherNumber.style.display = 'flex';
+    
+}
+
+function showBeer(){
+    optionTitle.innerHTML = 'Bebidas';
+    meatTitle.innerHTML = 'Quantidade de refrigerante:';
+    meatNumber.innerHTML = 'XXX L';
+    chickenTitle.style.display = 'flex';
+    chickenTitle.innerHTML = 'Quantidade de cerveja:';
+    chickenNumber.style.display = 'flex';
+    chickenNumber.innerHTML = 'XXX L';
+    otherTitle.style.display = 'none';
+    otherNumber.style.display = 'none';
+}
+
+function showPizza(){
+    optionTitle.innerHTML = 'Pizza';
+    meatTitle.innerHTML = 'Quantidade de pizza:';
+    meatNumber.innerHTML = 'XXX g';
+    chickenTitle.style.display = 'none';
+    chickenNumber.style.display = 'none';
+    otherTitle.style.display = 'none';
+    otherNumber.style.display = 'none';
+
 }
