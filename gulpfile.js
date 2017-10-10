742cb19bc18f9f25d42eb61f8d4524fa8efe7ea0
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-10-10 17:08:15 -0300

    Site versao 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,46 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const align = require('gulp-align');
+<<<<<<< HEAD
 const autoprefixer = require('gulp-autoprefixer');
 const connect = require('gulp-connect');
 const babel = require('babelify');
 const changed = require('gulp-changed');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+=======
+const notify = require('gulp-notify');
+const autoprefixer = require('gulp-autoprefixer');
+const connect = require('gulp-connect');
+const babel = require('gulp-babel');
+const changed = require('gulp-changed');
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+>>>>>>> Site versao 1.0
 
 gulp.task('sass', function () {
     gulp.src('src/sass/main.scss')
         .pipe(changed('public/css'))
         .pipe(sass())
         .pipe(autoprefixer({
+<<<<<<< HEAD
             browsers: ['last 2 versions', 'last 2 major versions', 'Firefox > 20', 'iOS 7', 'ie 6-8', 'last 2 iOS major versions'],
+=======
+            browsers: ['last 2 versions'],
+>>>>>>> Site versao 1.0
             cascade: false
         }))
         .pipe(cleanCSS())
         .pipe(gulp.dest('public/css'))
+<<<<<<< HEAD
+=======
+        .pipe(notify('SASS Salvo e modificado!'))
+>>>>>>> Site versao 1.0
         .pipe(connect.reload())
 });
 
 
+<<<<<<< HEAD
 gulp.task('minify-css', () => {
  return gulp.src('public/css/main.css')
    .pipe(cleanCSS({compatibility: 'ie8'}))
@@ -38,10 +57,30 @@ gulp.task('javasript', function () {
         .pipe(connect.reload())
 });
 
+=======
+gulp.task('javasript', function () {
+    gulp.src('src/js/*.js')
+        .pipe(changed('public/js/'))
+        .pipe(align())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('public/js/'))
+        .pipe(notify('JS Salvo e modificado!'))
+        .pipe(connect.reload())
+});
+
+
+>>>>>>> Site versao 1.0
 gulp.task('html', function () {
     gulp.src('src/*.html')
         .pipe(changed('public'))
         .pipe(gulp.dest('public/'))
+<<<<<<< HEAD
+=======
+        .pipe(notify('HTML Salvo e modificado!'))
+>>>>>>> Site versao 1.0
         .pipe(connect.reload())
 });
 
@@ -68,4 +107,8 @@ gulp.task('connect', function () {
 });
 
 
+<<<<<<< HEAD
 gulp.task('default', ['html', 'javasript', 'sass', 'minify-css', 'img', 'connect', 'watch']);
+=======
+gulp.task('default', ['html', 'javasript', 'sass', 'img', 'connect', 'watch']);
+>>>>>>> Site versao 1.0
