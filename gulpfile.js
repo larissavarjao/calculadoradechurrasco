0ac7038a2f6f02c5784e527f2791c0627c2bd029
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-10-17 14:37:42 -0300

    teste

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const align = require('gulp-align');
 <<<<<<< HEAD
+<<<<<<< HEAD
 const autoprefixer = require('gulp-autoprefixer');
 const connect = require('gulp-connect');
 const babel = require('babelify');
@@ -11,13 +12,18 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 =======
 const notify = require('gulp-notify');
+=======
+>>>>>>> Pendente resolver flebox auto-prefixer
 const autoprefixer = require('gulp-autoprefixer');
 const connect = require('gulp-connect');
-const babel = require('gulp-babel');
+const babel = require('babelify');
 const changed = require('gulp-changed');
-const jshint = require('gulp-jshint');
 const concat = require('gulp-concat');
+<<<<<<< HEAD
 >>>>>>> Site versao 1.0
+=======
+const uglify = require('gulp-uglify');
+>>>>>>> Pendente resolver flebox auto-prefixer
 
 gulp.task('sass', function () {
     gulp.src('src/sass/main.scss')
@@ -25,29 +31,40 @@ gulp.task('sass', function () {
         .pipe(sass())
         .pipe(autoprefixer({
 <<<<<<< HEAD
+<<<<<<< HEAD
             browsers: ['last 2 versions', 'last 2 major versions', 'Firefox > 20', 'iOS 7', 'ie 6-8', 'last 2 iOS major versions'],
 =======
             browsers: ['last 2 versions'],
 >>>>>>> Site versao 1.0
+=======
+            browsers: ['last 2 versions', 'last 2 major versions', 'Firefox > 20', 'iOS 7', 'ie 6-8', 'last 2 iOS major versions'],
+>>>>>>> Pendente resolver flebox auto-prefixer
             cascade: false
         }))
         .pipe(cleanCSS())
         .pipe(gulp.dest('public/css'))
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         .pipe(notify('SASS Salvo e modificado!'))
 >>>>>>> Site versao 1.0
+=======
+>>>>>>> Pendente resolver flebox auto-prefixer
         .pipe(connect.reload())
 });
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Pendente resolver flebox auto-prefixer
 gulp.task('minify-css', () => {
  return gulp.src('public/css/main.css')
    .pipe(cleanCSS({compatibility: 'ie8'}))
    .pipe(gulp.dest('public/css/main.minify.css'));
 });
 
+<<<<<<< HEAD
 gulp.task('javasript', function () {
     gulp.src('src/js/*.js')
         .pipe(changed('public/js/'))
@@ -58,29 +75,33 @@ gulp.task('javasript', function () {
 });
 
 =======
+=======
+>>>>>>> Pendente resolver flebox auto-prefixer
 gulp.task('javasript', function () {
     gulp.src('src/js/*.js')
         .pipe(changed('public/js/'))
-        .pipe(align())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
         .pipe(concat('script.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('public/js/'))
-        .pipe(notify('JS Salvo e modificado!'))
         .pipe(connect.reload())
 });
 
+<<<<<<< HEAD
 
 >>>>>>> Site versao 1.0
+=======
+>>>>>>> Pendente resolver flebox auto-prefixer
 gulp.task('html', function () {
     gulp.src('src/*.html')
         .pipe(changed('public'))
         .pipe(gulp.dest('public/'))
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         .pipe(notify('HTML Salvo e modificado!'))
 >>>>>>> Site versao 1.0
+=======
+>>>>>>> Pendente resolver flebox auto-prefixer
         .pipe(connect.reload())
 });
 
@@ -108,7 +129,11 @@ gulp.task('connect', function () {
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 gulp.task('default', ['html', 'javasript', 'sass', 'minify-css', 'img', 'connect', 'watch']);
 =======
 gulp.task('default', ['html', 'javasript', 'sass', 'img', 'connect', 'watch']);
 >>>>>>> Site versao 1.0
+=======
+gulp.task('default', ['html', 'javasript', 'sass', 'minify-css', 'img', 'connect', 'watch']);
+>>>>>>> Pendente resolver flebox auto-prefixer
