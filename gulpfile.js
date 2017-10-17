7e9a3314b8f3bacdfcc8f6d79a5f3bb601f3fa87
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-10-17 14:21:41 -0300

    Pendente resolver flebox auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,46 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const align = require('gulp-align');
-const notify = require('gulp-notify');
 const autoprefixer = require('gulp-autoprefixer');
 const connect = require('gulp-connect');
-const babel = require('gulp-babel');
+const babel = require('babelify');
 const changed = require('gulp-changed');
-const jshint = require('gulp-jshint');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
     gulp.src('src/sass/main.scss')
         .pipe(changed('public/css'))
         .pipe(sass())
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ['last 2 versions', 'last 2 major versions', 'Firefox > 20', 'iOS 7', 'ie 6-8', 'last 2 iOS major versions'],
             cascade: false
         }))
         .pipe(cleanCSS())
         .pipe(gulp.dest('public/css'))
-        .pipe(notify('SASS Salvo e modificado!'))
         .pipe(connect.reload())
 });
 
+
+gulp.task('minify-css', () => {
+ return gulp.src('public/css/main.css')
+   .pipe(cleanCSS({compatibility: 'ie8'}))
+   .pipe(gulp.dest('public/css/main.minify.css'));
+});
 
 gulp.task('javasript', function () {
     gulp.src('src/js/*.js')
         .pipe(changed('public/js/'))
-        .pipe(align())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
         .pipe(concat('script.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('public/js/'))
-        .pipe(notify('JS Salvo e modificado!'))
         .pipe(connect.reload())
 });
-
 
 gulp.task('html', function () {
     gulp.src('src/*.html')
         .pipe(changed('public'))
         .pipe(gulp.dest('public/'))
-        .pipe(notify('HTML Salvo e modificado!'))
         .pipe(connect.reload())
 });
 
@@ -70,4 +68,4 @@ gulp.task('connect', function () {
 });
 
 
-gulp.task('default', ['html', 'javasript', 'sass', 'img', 'connect', 'watch']);
+gulp.task('default', ['html', 'javasript', 'sass', 'minify-css', 'img', 'connect', 'watch']);
